718896eec35eca771ab188523e940834c34ac88e
Emilia Łukasiuk <lukasiuk.emilia@gmail.com>
2024-03-26 11:35:04 +0100

    tested utils/convertPLNToUSD and components/CurrencyForm, CurrencyForm to be fixed to pass test

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,21 @@
 export const convertPLNToUSD = (PLN) => {
 
-  const PLNtoUSD = PLN / 3.5;
-  
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
   });
 
+  if(typeof PLN == 'string' || PLN === undefined){
+    return NaN;
+  }
+  else if(typeof PLN !== 'string' && typeof PLN !== 'number'){
+    return 'Error';
+  }
+  else if(PLN < 0){
+    return formatter.format(0).replace(/\u00a0/g, ' ');
+  }
+  else{
+  const PLNtoUSD = PLN / 3.5;
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  }
 }
