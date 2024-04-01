cfdc4d832adf82692ec7690274a156ce5a5d2788
Emilia Łukasiuk <lukasiuk.emilia@gmail.com>
2024-04-01 12:41:05 +0200

    changing CurrencyForm test to use many testing cases

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -1,32 +1,44 @@
 import { render, screen } from '@testing-library/react';
 import CurrencyForm from './CurrencyForm.js';
 import userEvent from '@testing-library/user-event';
+import { cleanup } from '@testing-library/react';
+
+const testCases = [
+  { amount: '100', from: 'PLN', to: 'USD' },
+  { amount: '20', from: 'USD', to: 'PLN' },
+  { amount: '200', from: 'PLN', to: 'USD' },
+  { amount: '345', from: 'USD', to: 'PLN' },
+];
 
 describe('Component CurrencyForm', () => {
-  it('should run action callback with proper data on form submit', () => {
+  for(const testObj of testCases) {
+    it('should run action callback with proper data on form submit', () => {
     const action = jest.fn();
-  
+
     // render component
     render(<CurrencyForm action={action} />);
-  
+
     // find “convert” button
     const submitButton = screen.getByText('Convert');
-  
+
     // find fields elems
     const amountField = screen.getByTestId('amount');
     const fromField = screen.getByTestId('from-select');
     const toField = screen.getByTestId('to-select');
-  
+
     // set test values to fields
-    userEvent.type(amountField, '100');
-    userEvent.selectOptions(fromField, 'PLN');
-    userEvent.selectOptions(toField, 'USD');
-  
+    userEvent.type(amountField, testObj.amount);
+    userEvent.selectOptions(fromField, testObj.from);
+    userEvent.selectOptions(toField, testObj.to);
+
     // simulate user click on "convert" button
     userEvent.click(submitButton);
-  
+
     // check if action callback was called once and with proper argument
     expect(action).toHaveBeenCalledTimes(1);
-    expect(action).toHaveBeenCalledWith({ amount: 100, from: 'PLN', to: 'USD' });
-  });
+    expect(action).toHaveBeenCalledWith({ amount: parseInt(testObj.amount), from: testObj.from, to: testObj.to });
+    });
+    // unmount component
+    cleanup()
+  }
 });
